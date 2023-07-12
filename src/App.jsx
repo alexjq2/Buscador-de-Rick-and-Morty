0d60c41ea7d85e9036a0ef93e500d4f3642890af
alex <alexqcanchan@gmail.com>
2023-07-12 02:49:40 -0500

    avanc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-
+import Location from "./Components/Location"
+import logo from "./assets/logo.svg"
 function App() {
 
   return (
-    <>
-      
-    </>
+    <section className='app'>
+      <div className='banner'>
+        <img src={logo} alt="" />
+        <Location/>
+      </div>
+      <div className='footer'/>
+    </section>
   )
 }
 
